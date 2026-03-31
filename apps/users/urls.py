35e3be90180a02c954c01d8c49162e45686044bf
jason Acharya <acharyajason12@gmail.com>
2026-03-31 17:12:39 +0545

    Users App: Added forgot password and reset password functionality

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,10 +1,19 @@
 
 from django.urls import path
-from .views import RegisterView, LoginView, LogoutView, UserProfileView
+from .views import (
+    RegisterView, 
+    LoginView, 
+    LogoutView, 
+    UserProfileView,
+    ForgotPasswordView,
+    ResetPasswordView
+)
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/', UserProfileView.as_view(), name='profile'),
+    path('forgot-password/', ForgotPasswordView.as_view(), name='forgot-password'),
+    path('reset-password/', ResetPasswordView.as_view(), name='reset-password'),
 ]
