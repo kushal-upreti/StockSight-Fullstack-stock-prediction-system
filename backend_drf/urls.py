723425e2f09f8eb024ed94e60c1fa387e09ab646
kushal-upreti <kushalupreti66@gmail.com>
2026-07-03 18:17:01 +0545

    updated again

--- backend_drf/urls.py
+++ backend_drf/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('apps.users.urls')),
+    path('api/', include('apps.stocks.urls')),
     path('api/prediction/', include('apps.predictions.urls')),
     path('api/bookmarks/', include('apps.bookmarks.urls')),
     path('api/payments/', include('apps.payments.urls')),
